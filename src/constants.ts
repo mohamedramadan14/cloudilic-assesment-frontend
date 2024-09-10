3d5ed46adf955a711736137558f7b4d578fc0e76
Mohamed Ramadan <mohameda7maa@gmail.com>
2024-09-10 15:56:55 +0300

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
 export const BACKEND_URL_LOCAL = "http://localhost:8000"
-export const BACKEND_URL_PROD = "https://useful-rivalee-solo-mo-a13a996e.koyeb.app";
+export const BACKEND_URL_PROD = "https://optimistic-pheasant-tuts-165dfa54.koyeb.app";
